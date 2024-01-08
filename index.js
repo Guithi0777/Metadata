451442ddaff5291c085133c88d1e96f47afb55d3
THIBAULT.G <144021714+Guithi0777@users.noreply.github.com>
2024-01-08 23:41:01 +0100

    essayons !!!

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
-const core = require('@actions/core');
-const exec = require("@actions/exec");
-
-async function run() {
-    try {
-        const versionType = core.getInput('version-type');
-        const src = __dirname;
-
-        await exec.exec(`${src}/git_update.sh -v ${versionType}`);
-    } catch (error) {
-        core.setFailed(error.message);
-    }
-}
-
-run();
+console.log('Rien qu un test')
